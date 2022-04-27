8671d480ac855bc258c4c1c286d60958c228c6c4
Shubham Kumar Verma <84174011+vermashubham6351@users.noreply.github.com>
2022-04-27 18:35:54 +0530

    Added solution - LeetHub

---
+++ Killing Spree - GFG/killing-spree.cpp
@@ -0,0 +1,38 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution {
+public:
+    long long int killinSpree(long long int n)
+    
+    {
+        // Code Here
+        long long int i=0;
+        for(i=1;i*i<=n;i++)
+            n-=(i*i);
+        return i-1;
+    }
+        
+    
+};
+
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int n; cin>>n;
+        Solution obj;
+        long long int ans = obj.killinSpree(n);
+        cout<<ans<<"\n";
+    }
+    return 0;
+}
+  // } Driver Code Ends
