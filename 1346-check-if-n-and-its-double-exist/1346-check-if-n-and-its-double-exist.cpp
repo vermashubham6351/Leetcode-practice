349af21d883d6dabb1ba495ac68d9ff3d8d7b54a
Shubham Kumar Verma <84174011+vermashubham6351@users.noreply.github.com>
2022-05-03 13:14:34 +0530

    Time: 4 ms (91.66%), Space: 10.3 MB (36.53%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+     bool checkIfExist(vector<int>& arr) {
+        unordered_set<int> uset;
+        for(int& i:arr){
+            if(uset.count(2*i) || (!(i&1) && (uset.count(i/2))))
+                return true;
+            uset.insert(i);
+        }
+        return false;
+    }
+};
